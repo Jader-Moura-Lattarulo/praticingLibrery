c179c68acde0c6bfe9efc850f44b32be04546b74
Jader Moura <jader.dev.moura@gmail.com>
2024-11-09 16:32:36 -0300

    Exercise 01 done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import com.google.gson.Gson;
+
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        Gson gson = new Gson();
+
+        FileReader reader = new FileReader("src/personJson.json");
+
+        PersonRecord person = gson.fromJson(reader, PersonRecord.class);
+
+        System.out.printf("""
+                Nome: %s
+                Idade: %d
+                Cidade: %s""", person.name(), person.age(), person.city());
+
+        reader.close();
+
     }
 }
---
+++ src/PersonRecord.java
@@ -0,0 +1,1 @@
+public record PersonRecord(String name, int age, String city) {}
